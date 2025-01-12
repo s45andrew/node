9216c60d574a8a0cedf5c269cd7f35dc96e4adfc
s45andrew <s45andrew@googlemail.com>
2025-01-12 22:53:07 +0000

    @res or reb basic

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const http = require('http');
 
 const server = http.createServer((req,res) => {
-   console.log('request made');
+   console.log(req.url,req.method);
+
+   //setheader content type
+   res.setHeader('content-type', 'text/html');
+   res.write('<head><link rel="stylesheet" href="#"</head>');
+   res.write('<p>hello ninjas from my node</p>')
+   res.write('<h1>hello ninjas from my node</h1>')
+   res.end();
 });
 
 server.listen(3000, 'localhost', () => {
